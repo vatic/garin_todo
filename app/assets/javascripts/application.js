b7accae718df2b8e6a92d3b747df05e3132214bf
Sergey Vatagin <s.vatagin@gmail.com>
2015-11-18 14:56:25 +0300

    add bootstrap css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
 //= require jquery_ujs
+//= require jquery
+//= require bootstrap-sprockets
+
 //= require_tree .
+
