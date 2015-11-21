da977da7776a8198e171d6a939384c5bf76dbb9f
Sergey Vatagin <s.vatagin@gmail.com>
2015-11-22 00:01:17 +0300

    add edit todo button js listener

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,2 +1,13 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
+$( document ).ready(function() {
+  var todosApp = function() {
+    console.log('todos.js loaded...');
+    $('.todo-edit-button').on('click', function(e) {
+      var todo_id = $(e.target).data('todo_id');
+      console.log('click', todo_id);
+      console.log('click form', $('form#edit_todo_' + todo_id));
+      $('.todo-edit-form').addClass('hidden');
+      $('form#edit_todo_' + todo_id).removeClass('hidden');
+    });
+  }();
+}); 
