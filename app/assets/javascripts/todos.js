fb315414f62b0a47538ce5bb2257f7d116aae19b
Sergey Vatagin <s.vatagin@gmail.com>
2015-11-22 01:03:39 +0300

    move submit edit form to js

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -2,12 +2,23 @@
 $( document ).ready(function() {
   var todosApp = function() {
     console.log('todos.js loaded...');
+
     $('.todo-edit-button').on('click', function(e) {
       var todo_id = $(e.target).data('todo_id');
-      console.log('click', todo_id);
-      console.log('click form', $('form#edit_todo_' + todo_id));
-      $('.todo-edit-form').addClass('hidden');
-      $('form#edit_todo_' + todo_id).removeClass('hidden');
+      $('.todo-edit-form-wrapper').addClass('hidden');
+      $('.todo-edit-form-wrapper#' + todo_id).removeClass('hidden');
+    });
+
+    $('.todo-edit-form-cancel-button').on('click', function(e) {
+      var todo_id = $(e.target).data('todo_id');
+      console.log('click cancel', todo_id);
+      $('.todo-edit-form-wrapper#' + todo_id).addClass('hidden');
+    });
+
+    $('.todo-edit-form-submit-button').on('click', function(e) {
+      var todo_id = $(e.target).data('todo_id');
+      console.log('click submit', todo_id);
+      $('#edit_todo_' + todo_id).submit();
     });
   }();
 }); 
