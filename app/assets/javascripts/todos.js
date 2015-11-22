71275e7ffaa607acba96770556b121eb710e3f0f
Sergey Vatagin <s.vatagin@gmail.com>
2015-11-22 13:10:41 +0300

    add new todo form show

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -5,12 +5,14 @@ $( document ).ready(function() {
 
     $('.todo-edit-button').on('click', function(e) {
       var todo_id = $(e.target).data('todo_id');
+      console.log('todo-edit-button click', '.todo-edit-form-wrapper#' + todo_id);
       $('.todo-edit-form-wrapper').addClass('hidden');
       $('.todo-edit-form-wrapper#' + todo_id).removeClass('hidden');
     });
 
     $('.todo-new-button').on('click', function(e) {
-      $('.todo-new-form-wrapper').removeClass('hidden');
+      console.log('click new button', e);
+      $('#new-todo').removeClass('hidden');
     });
 
     $('.todo-edit-form-cancel-button').on('click', function(e) {
@@ -20,10 +22,15 @@ $( document ).ready(function() {
     });
 
     $('.todo-edit-form-submit-button').on('click', function(e) {
-      var todo_id = $(e.target).data('todo_id');
+      var todo_id = $(e.target).data('todo_id') || null;
       console.log('click submit', todo_id);
-      var formData = $('#edit_todo_' + todo_id).serialize();
-      var url = $('.todo-edit-form-wrapper#' + todo_id).data('url');
+      if (todo_id) {
+        var formData = $('#edit_todo_' + todo_id).serialize();
+        var url = $('.todo-edit-form-wrapper#' + todo_id).data('url');
+      } else {
+        var formData = $('#new_todo').serialize();
+        var url = $('.todo-edit-form-wrapper#new-todo').data('url');
+      }
       console.log('serialized data', formData);
       console.log('form url', url);
       $.post(url, formData)
